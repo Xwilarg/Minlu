f1af112f5ac307edf6f51bc43cdb71509c3737fe
Xwilarg <xwilarg@protonmail.com>
2025-12-27 00:55:11 +0100

    Add step listener

--- app/src/main/java/eu/zirk/minlu/StepService.kt
+++ app/src/main/java/eu/zirk/minlu/StepService.kt
@@ -1,14 +1,18 @@
 package eu.zirk.minlu
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class StepService : Service() {
 
@@ -17,6 +21,8 @@ class StepService : Service() {
     }
     private val sensor: Sensor? by lazy {
         sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) }
+
+    private lateinit var listener: SensorEventListener
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -32,6 +38,33 @@ class StepService : Service() {
             100,
             notification
         )
+
+        registerStepListener()
+
         return START_STICKY
     }
+
+    private fun registerStepListener() {
+        listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                if (event == null) return
+                val steps = event.values[0].toLong()
+                Log.d(TAG, "Steps since last reboot: $steps")
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                Log.d(TAG, "Accuracy changed to: $accuracy")
+            }
+        }
+
+        sensor?.let {
+            sensorManager.registerListener(listener, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(listener)
+    }
 }
+private const val TAG = "STEP_COUNT_LISTENER"
